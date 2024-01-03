39016c95eceed08dd03f473e241fdcae7ba8305f
yungan9 <geyun105@naver.com>
2024-01-03 17:49:57 +0900

    Day 2 : js 기본 문법

--- Day1.js
+++ Day1.js
@@ -1,1 +1,61 @@
-return 0;
+//Math
+Math.pow(3, 2); //제곱
+Math.sqrt; //제곱근
+Math.round; //반올림
+Math.ceil; //소수점이 있는 경우, 위의 정수로 반올림
+Math.floor; //소수점이 있는 경우, 가까운 쪽에 있는 아래 정수로 반내림
+Math.random; //난수(1보다 작은 수)
+
+//문자
+typeof //데이터 형식 알아보기
+1 //num
+"2" //String
+
+//문자 명령어
+\n //줄바꿈
+n + n //숫자일 경우 연산
+"" + "" //String이기 때문에 이어서 출력
+.length //문자열의 길이 출력
+.indexOf //문자열의 리스트값 출력
+
+//변수
+var //유효범위 변수 선언
+
+//비교 연산자
+== //동등 연산자
+=== //일치 연산자 (데이터 형이 같아야 함)
+!= //부정
+!== //정확하게 같지 않다
+>, <, >=, <=
+
+Boolean true/false
+Number 
+String 
+undefined //값이 없는 상태
+null //값이 없음을 명시적으로 표시
+
+//조건문
+if (Boolean){
+} else if(Boolean){
+
+} else{
+
+}
+
+//논리연산자
+&& //and
+|| //or
+! //부정
+
+//반복문
+while(1){
+
+}
+
+for(;;){
+
+}
+
+break;
+continue;
+
