a176bad0ee5b6daecc52cb2e955c10489b0a90b3
yungan9 <geyun105@naver.com>
2024-01-05 09:41:54 +0900

    Day 3 : 함수 개념 정리

---
+++ Day2.js
@@ -0,0 +1,18 @@
+//함수
+function name(arg) {
+  //방법 1
+  return 0;
+}
+name(); //함수 호출
+//마지막 return 후 함수 실행 종료
+
+number = function () {
+  //방법 2
+  return 0;
+};
+number(); //함수 호출
+
+(function () {
+  //익명 함수 : 일회성 호출
+  return 0;
+})();
