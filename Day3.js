581e4564d58cba043faf17a8992decad90c3125c
yungan9 <geyun105@naver.com>
2024-01-05 11:41:38 +0900

    Day 3 : 배열 메소드 정리

---
+++ Day3.js
@@ -0,0 +1,12 @@
+//배열
+var num = [one, two, three];
+
+a.toUpperCase(); //대문자 변경
+a.push(); //배열 끝에 원소 추가
+a.concat(); //원소 여러개 추가
+a.unshift(); //배열 앞에 원소 추가
+a.splice(idx, -num, num); //인덱스에 들어갈 값 (인덱스,인덱스 뒤 제거될 원소수,값)
+a.shift(); //첫번째 원소 제거
+a.pop(); //마지막 원소 제거
+a.sort(); //정렬
+a.reverse(); //역순 정렬
