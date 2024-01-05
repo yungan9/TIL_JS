d124805243769cc6749c9eb354adc0b2b0e18b59
yungan9 <geyun105@naver.com>
2024-01-05 16:21:11 +0900

    Day 3 : 객체 이해

--- Day3.js
+++ Day3.js
@@ -1,5 +1,5 @@
 //배열
-var num = [one, two, three];
+var arr = [one, two, three];
 
 a.toUpperCase(); //대문자 변경
 a.push(); //배열 끝에 원소 추가
@@ -10,3 +10,24 @@ a.shift(); //첫번째 원소 제거
 a.pop(); //마지막 원소 제거
 a.sort(); //정렬
 a.reverse(); //역순 정렬
+
+//객체
+var idx = { key: value }; //방법 1
+
+var idx = {}; //방법 2
+idx["key"] = value;
+
+var idx = new Object(); //방법 3
+idx["key"] = value;
+new Object();
+
+idx["key"]; //객체 속성 접근 방법 1
+idx.key; //객체 속성 접근 방법 2
+
+var idx = {
+  key: { key: value, key: value },
+  fuc: function () {
+    alert(this); //속해있는 객체를 가리키는 변수
+  },
+};
+idx["fuc"]();
