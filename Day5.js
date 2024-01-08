ed1ef6150d60646a9ae411a9e41f4c63a338fbd5
yungan9 <geyun105@naver.com>
2024-01-08 11:08:18 +0900

    Day 5 : 정규표현식

---
+++ Day5.js
@@ -0,0 +1,22 @@
+//정규표현식
+/*문자열에서 특정한 문자를 찾아내는 도구
+  텍스트 안의 정보 존재 여부를 찾거나 
+  텍스타 안의 정보 중에서 패턴에 해당하는 다른 텍스트로 치환
+  이러한 경우에 정규 표현식 사용*/
+//컴파일 단계
+var pattern = /a/; //정규표현식 리터럴
+var pattern = new RegExp("a"); //정규표현식 객체 생성자
+//RegExp는 정규표현식 객체
+RegExp.exec(); //찾는 문자열이 있으면 값 리턴
+pattern.exec("abcde"); //["a"]
+
+RegExp.test(); //문자열이 있으면 true, 없으면 false
+
+String.match(); //문자열 메소드 실행
+str.match(pattern); //["a"]
+
+String.replace(); //문자열 패턴 검색 후 변경하여 값 리턴
+str.replace(pattern, "A"); //"Abcde"
+
+var oi = /a/i; //i를 주게되면 대소문자 구분 x
+var xi = /a/g; //g를 주게되면 검색된 모든 결과(개수) 리턴
